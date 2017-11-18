e6e8091468901f18ded5a3db79d093911cf9c8ff
AlexDayCodes <33786354+AlexDayCodes@users.noreply.github.com>
2017-11-18 15:43:23 +0000

    Added more comments

--- Midfield.R
+++ Midfield.R
@@ -1,42 +1,57 @@
+# Create vectors of touhes in final third
 a <- c(64, 46, 45, 37, 40, 63, 41, 32, 36, 61, 31) # Kevin De Bruyne
 b <- c(25, 16, 21, 6, 10, 19, 13, 13, 8, 21, 11) # Fernando
 c <- c(54, 36, 40, 24, 39, 56, 38, 42, 40, 55, 41) # David Silva
 
+# Create dataframe of vectos
 my.df <- data.frame(Kevin.De.Bruyne = a, Fernando = b, David.Silva = c)
+
+# Print summary of dataframe
 summary(my.df)
 
+# Run ANOVA test on dataframe
 test.aov <- aov(formula = Kevin.De.Bruyne ~ Fernando + David.Silva, data = my.df)
+
+# Print summary of ANOVA
 summary(test.aov)
 
+# Plot data
 plot(a,c, xlim = c(0,3), ylim = c(0, 70), type = "n", xaxt="n", yaxs = "i", main = "Touches in Final Third", xlab = "", ylab = "")
-rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],col = "lightskyblue1")
 
+# Set background colour
+rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],col = "lightskyblue1")
 my.col.a <- "darkorchid3"
 my.col.b <- "coral3"
 my.col.c <- "green4"
 
+# Add raw points
 points(rnorm(length(a),0.5 , 0.05), a, pch = 1, col = my.col.a)
 points(rnorm(length(b),1.5 , 0.05), b, pch = 1, col = my.col.b)
 points(rnorm(length(c),2.5 , 0.05), c, pch = 1, col = my.col.c)
 axis(side = 1, at = c(0.5, 1.5, 2.5), labels = c("Kevin De Bruyne", "Fernando", "David Silva")) 
 
+# Find density
 density.a <- density(a)
 
+# Adjust density to scale and omit outliers
 a.plot.ma <- matrix(nrow = length(density.a$x), ncol = 3)
 a.plot.ma[, 1] <- density.a$x
 a.plot.ma[, 2] <- 0.5 + (density.a$y * 0.3 / max(density.a$y))
 a.plot.ma[, 3] <- 0.5 - (density.a$y * 0.3 / max(density.a$y))
-
 a.plot.ma <- subset(a.plot.ma, a.plot.ma[, 1] > quantile(a)[[1]] & a.plot.ma[, 1] < quantile(a)[[5]])
 
+# Add density lines around raw points
 lines(a.plot.ma[,2], a.plot.ma[,1], col = "darkorchid3")
 lines(a.plot.ma[,3], a.plot.ma[,1], col = "darkorchid3")
 
+# Add mean line
 segments(min(a.plot.ma[,3]), mean(a), max(a.plot.ma[,2]), mean(a), lwd = 4, col = "darkorchid")
 
+# Tidy up density ends
 segments(a.plot.ma[1,2], a.plot.ma[1,1], a.plot.ma[1,3], a.plot.ma[1,1], lwd = 1, col = "darkorchid")
 segments(a.plot.ma[nrow(a.plot.ma),2], a.plot.ma[nrow(a.plot.ma),1], a.plot.ma[nrow(a.plot.ma),3], a.plot.ma[nrow(a.plot.ma),1], lwd = 1, col = "darkorchid")
 
+# Repeat for other players
 density.b <- density(b)
 
 b.plot.ma <- matrix(nrow = length(density.b$x), ncol = 3)
